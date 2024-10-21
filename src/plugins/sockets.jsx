fab4276ef8050603adf103afb0f005ec806c1cf8
unknown <tomasjarutis@gmail.com>
2024-10-21 22:03:11 +0300

    socket

--- src/plugins/sockets.jsx
+++ src/plugins/sockets.jsx
@@ -1,2 +1,3 @@
 import { io } from 'socket.io-client';
-export const socket = io("http://localhost:2000");
+// export const socket = io("http://localhost:2000");
+export const socket = io("https://final-task-back1.onrender.com");
